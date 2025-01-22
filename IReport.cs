4497b09698e3babaca90cac28a1be25e27af72d5
OrnarasUS <ornaras.us@gmail.com>
2025-01-22 09:34:54 +0400

    Перенос инициализации отчёта в основное приложение

---
+++ IReport.cs
@@ -0,0 +1,22 @@
+﻿namespace RetailCorrector.API;
+
+/// <summary>
+/// Тип отчета
+/// </summary>
+public interface IReport
+{
+    /// <summary>
+    /// Идентификатор отчета
+    /// </summary>
+    public string Id { get; }
+
+    /// <summary>
+    /// Параметры отчёта
+    /// </summary>
+    public Dictionary<string, string> Properties { get; }
+
+    /// <summary>
+    /// Отправка отчета
+    /// </summary>
+    public Task Send();
+}
